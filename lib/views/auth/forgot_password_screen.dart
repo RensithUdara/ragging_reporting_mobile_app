8b3e568c42f7561f250060b8deeeeb7d5c134db4
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-08-24 21:00:34 +0530

    refactor: simplify navigation in ForgotPasswordScreen by replacing pushReplacement with pop

--- lib/views/auth/forgot_password_screen.dart
+++ lib/views/auth/forgot_password_screen.dart
@@ -206,11 +206,7 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
         CustomButton(
           text: 'Back to Login',
           onPressed: () {
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(
-                builder: (context) => const LoginScreen(),
-              ),
-            );
+            Navigator.of(context).pop();
           },
         ),
       ],
